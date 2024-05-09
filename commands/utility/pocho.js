cb538c82bca4a076a750d54f118cda8e90b3db10
sem1312 <108529990+sem1312@users.noreply.github.com>
2024-05-09 05:50:58 -0600

    nuevos comandos utiles

---
+++ commands/utility/pocho.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('pochin')
+		.setDescription('el bea one'),
+	async execute(interaction) {
+		await interaction.reply(`SEXY DOG`);
+	},
+};
